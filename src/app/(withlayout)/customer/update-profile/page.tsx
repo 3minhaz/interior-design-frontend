cd77627d3939effa856e77eb4165787c0940c5d9
3minhaz <3minhaz@gmail.com>
2023-10-18 23:05:04 +0600

    dashboard work done

---
+++ src/app/(withlayout)/customer/update-profile/page.tsx
@@ -0,0 +1,97 @@
+"use client";
+
+import CustomInput from "@/components/Forms/CustomInput";
+import Form from "@/components/Forms/Form";
+import UMBreadCrumb from "@/components/ui/BreadCrumb";
+import {
+  useMyProfileQuery,
+  useUpdateMyProfileMutation,
+} from "@/redux/api/userApi";
+import { useVerifyUser } from "@/utils/customHooks";
+import { Button, Col, Row, message } from "antd";
+
+const UpdateProfilePage = () => {
+  useVerifyUser("customer");
+  const { data, isLoading } = useMyProfileQuery("");
+
+  const [updateMyProfile] = useUpdateMyProfileMutation();
+  const onSubmit = async (data: any) => {
+    try {
+      const res = await updateMyProfile({ body: data });
+      // @ts-ignore
+      if (res?.data?.id) {
+        message.success("User data updated successfully");
+      }
+    } catch (err: any) {
+      console.log(err, "checking error");
+      message.error(err.message);
+    }
+  };
+  const defaultValues = {
+    address: data?.address || "",
+    contactNo: data?.contactNo || "",
+    name: data?.name || "",
+  };
+  return (
+    <div>
+      <UMBreadCrumb
+        items={[
+          {
+            label: "Customer",
+            link: "/customer",
+          },
+        ]}
+      />
+      <h1>Update user data</h1>
+      <Form submitHandler={onSubmit} defaultValues={defaultValues}>
+        <Row gutter={{ xs: 24, xl: 8, lg: 8, md: 24 }}>
+          <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
+            <Col
+              className="gutter-row"
+              span={8}
+              style={{
+                marginBottom: "10px",
+              }}
+            >
+              <CustomInput
+                type="text"
+                name="address"
+                size="large"
+                label="Address"
+              />
+            </Col>
+            <Col
+              className="gutter-row"
+              span={8}
+              style={{
+                marginBottom: "10px",
+              }}
+            >
+              <CustomInput
+                type="text"
+                name="contactNo"
+                size="large"
+                label="Contact No"
+              />
+            </Col>
+            <Col
+              className="gutter-row"
+              span={8}
+              style={{
+                marginBottom: "10px",
+              }}
+            >
+              <CustomInput type="text" name="name" size="large" label="Name" />
+            </Col>
+          </Row>
+        </Row>
+
+        <Button type="primary" htmlType="submit">
+          Update your profile
+        </Button>
+      </Form>
+    </div>
+  );
+};
+
+export default UpdateProfilePage;
---
+++ src/app/(withlayout)/super_admin/update-profile/page.tsx
@@ -0,0 +1,99 @@
+"use client";
+
+import CustomInput from "@/components/Forms/CustomInput";
+import Form from "@/components/Forms/Form";
+import UMBreadCrumb from "@/components/ui/BreadCrumb";
+import {
+  useMyProfileQuery,
+  useUpdateMyProfileMutation,
+} from "@/redux/api/userApi";
+import { useVerifyUser } from "@/utils/customHooks";
+import { Button, Col, Row, message } from "antd";
+
+const UpdateProfilePage = () => {
+  useVerifyUser("super_admin");
+  const { data, isLoading } = useMyProfileQuery("");
+
+  const [updateMyProfile] = useUpdateMyProfileMutation();
+  const onSubmit = async (data: any) => {
+    try {
+      const res = await updateMyProfile({ body: data });
+      // @ts-ignore
+      if (res?.data?.id) {
+        message.success("User data updated successfully");
+      }
+    } catch (err: any) {
+      message.error(err.message);
+    }
+  };
+  const defaultValues = {
+    address: data?.address || "",
+    contactNo: data?.contactNo || "",
+    name: data?.name || "",
+    // location: data?.location || "",
+    // price: data?.price || "",
+    // serviceStatus: data?.serviceStatus || "",
+  };
+  return (
+    <div>
+      <UMBreadCrumb
+        items={[
+          {
+            label: "super admin",
+            link: "/super_admin",
+          },
+        ]}
+      />
+      <h1>Update user data</h1>
+      <Form submitHandler={onSubmit} defaultValues={defaultValues}>
+        <Row gutter={{ xs: 24, xl: 8, lg: 8, md: 24 }}>
+          <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
+            <Col
+              className="gutter-row"
+              span={8}
+              style={{
+                marginBottom: "10px",
+              }}
+            >
+              <CustomInput
+                type="text"
+                name="address"
+                size="large"
+                label="Address"
+              />
+            </Col>
+            <Col
+              className="gutter-row"
+              span={8}
+              style={{
+                marginBottom: "10px",
+              }}
+            >
+              <CustomInput
+                type="text"
+                name="contactNo"
+                size="large"
+                label="Contact No"
+              />
+            </Col>
+            <Col
+              className="gutter-row"
+              span={8}
+              style={{
+                marginBottom: "10px",
+              }}
+            >
+              <CustomInput type="text" name="name" size="large" label="Name" />
+            </Col>
+          </Row>
+        </Row>
+
+        <Button type="primary" htmlType="submit">
+          Update your profile
+        </Button>
+      </Form>
+    </div>
+  );
+};
+
+export default UpdateProfilePage;
